0154c3bc4f0f13f29ff66c471050afe01424688b
shreejutanna <shreejuj@buffalo.edu>
2017-04-17 13:03:48 -0400

    Add files via upload

---
+++ filescript2.py
@@ -0,0 +1,55 @@
+import csv
+import shutil
+from pathlib import Path
+
+	
+import tensorflow as tf
+import sys
+
+with open('test_data.csv', newline='') as csvfile:
+	count=0;
+	paths=[];
+	csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+	for row in csvreader:
+		csvstrings=(row[0].split(","));
+		my_file = Path("images/"+csvstrings[0]+".jpg");
+		if my_file.is_file():
+			count+=1;
+			paths.append("images/"+csvstrings[0]+".jpg");
+
+	print(count);
+
+	# Loads label file, strips off carriage return
+label_lines = [line.rstrip() for line 
+       		in tf.gfile.GFile("WalMart project/training/retrained_labels.txt")]
+
+# Unpersists graph from file
+with tf.gfile.FastGFile("WalMart project/training/retrained_graph.pb", 'rb') as f:
+	graph_def = tf.GraphDef()
+	graph_def.ParseFromString(f.read())
+	_ = tf.import_graph_def(graph_def, name='')
+
+with tf.Session() as sess:
+	# Feed the image_data as input to the graph and get first prediction
+	softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+
+	for image_path in paths :
+
+		# Read in the image_data
+		image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+		predictions = sess.run(softmax_tensor, \
+			{'DecodeJpeg/contents:0': image_data})
+
+		# Sort to show labels of first prediction in order of confidence
+		top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+		print (top_k);
+
+		with open('results.csv', 'a', newline='') as csvfile:
+			csvwriter = csv.writer(csvfile, delimiter=' ',
+				quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			if(top_k[0]==1):
+				csvwriter.writerow([image_path[7:-4],'TRUE', str(predictions[0][top_k[0]])])
+			else:
+				csvwriter.writerow([image_path[7:-4],'FALSE', str(predictions[0][top_k[1]])])
+			
